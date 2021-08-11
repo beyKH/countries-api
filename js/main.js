27f12716f47fc1185d20bfc9465b93dc7632215b
AsadbekMakhmudjonov <asadbek.mah.@gmail.com>
2021-08-11 09:02:07 +0500

    all finished

---
+++ js/main.js
@@ -0,0 +1,5 @@
+var elSitenavToggler = document.querySelector(".site-header__toggler");
+
+elSitenavToggler.addEventListener("click", function () {
+  document.body.classList.toggle("body--open");
+})
